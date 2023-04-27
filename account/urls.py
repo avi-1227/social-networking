b3de7c97d3f43ac8965ddf6f0831d52292dfce24
avi-1227 <avinaashprasadd@gmail.com>
2023-04-27 22:10:43 +0530

    Update url for signup view

--- account/urls.py
+++ account/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from .views import SignUpView
-from . import views
+from . import views 
 
 
 urlpatterns = [
     path("", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("dashboard/", views.dashboard, name="dashboard"),
-    path("signup/", SignUpView.as_view(), name='signup'),
+    path("signup/", views.sign_up, name='signup'), # Update URL Path
 ]
